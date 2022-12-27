aaeeb03fad4f59fe6b664bee0c803a2a196a46d2
Lowkeyoj <rockyojaji@gmail.com>
2022-12-26 17:21:42 -0800

    got the game to randomly give the computer choices

--- java.js
+++ java.js
@@ -1,22 +1,35 @@
 const gameboard = document.querySelector('#gameboard');
 
 
-/* const gamplay = (function(){
+ const gamplay = (function(){
  
  const player = {player1: 'x', player2: '0',}
  const toText = function(div){
     div.addEventListener('click',function(){ div.textContent = player.player1;
     
-    document.querySelector(`[data = "${Math.floor(Math.random() * 10)}"]`).textContent = player.player2 });
+        const computer = function(){const computerchoice = document.querySelector(`[data = "${Math.floor(Math.random() * 10)}"]`);
+        
+        if(!computerchoice){return computer();};
+        if(computerchoice === div){
+            return computer();
+        };if(computerchoice.textContent === player.player1){
+            return computer();
+        };
+        if(computerchoice.textContent === player.player2){return computer()};
+        
+        console.log(computerchoice);  
+        return computerchoice.textContent = player.player2 }
+        
+         computer(); 
+    
+    });
     
     return div.textContent};
  const matrix = Array.from(gameboard.children).map(toText);
   
  return{matrix,player}
 }
-)(); */
+)(); 
  
-(function me(){const test = document.querySelector(`[data = "${Math.floor(Math.random() * 10)}"]`);
- if(test){ return console.log(test)};
-  return me();  })()
+
 
