e530727362772bbc086938650139757681cf4d13
Lowkeyoj <rockyojaji@gmail.com>
2022-12-26 16:35:00 -0800

    used some recursion to avoidan error

--- java.js
+++ java.js
@@ -1,18 +1,22 @@
 const gameboard = document.querySelector('#gameboard');
 
 
-const gamplay = (function(){
+/* const gamplay = (function(){
+ 
  const player = {player1: 'x', player2: '0',}
  const toText = function(div){
-    div.addEventListener('click',() => div.textContent 
-    = `${player.player1}` || `${player.player2}`);
+    div.addEventListener('click',function(){ div.textContent = player.player1;
+    
+    document.querySelector(`[data = "${Math.floor(Math.random() * 10)}"]`).textContent = player.player2 });
     
     return div.textContent};
  const matrix = Array.from(gameboard.children).map(toText);
   
  return{matrix,player}
 }
-)();
+)(); */
  
-
+(function me(){const test = document.querySelector(`[data = "${Math.floor(Math.random() * 10)}"]`);
+ if(test){ return console.log(test)};
+  return me();  })()
 
