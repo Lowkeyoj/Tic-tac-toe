79a4067d979ba9fcc456fde95a35e87eddcc2e4f
Lowkeyoj <rockyojaji@gmail.com>
2022-12-26 18:53:07 -0800

    gamplay factory basicly done few but to fix

--- java.js
+++ java.js
@@ -2,11 +2,20 @@ const gameboard = document.querySelector('#gameboard');
 
 
  const gamplay = (function(){
- 
+ const update = function(){ let newMatrix = Array.from(document.querySelector('#gameboard').children);
+   function toText2(div){ return div.textContent};
+   newMatrix = newMatrix.map(toText2);
+   return newMatrix; }
  const player = {player1: 'x', player2: '0',}
  const toText = function(div){
-    div.addEventListener('click',function(){ div.textContent = player.player1;
     
+    div.addEventListener('click',function(){ 
+        if((div.textContent === player.player1) || (div.textContent === player.player2)){
+            return;
+        }
+        
+        div.textContent = player.player1;
+        
         const computer = function(){const computerchoice = document.querySelector(`[data = "${Math.floor(Math.random() * 10)}"]`);
         
         if(!computerchoice){return computer();};
@@ -21,15 +30,21 @@ const gameboard = document.querySelector('#gameboard');
         return computerchoice.textContent = player.player2 }
         
          computer(); 
-    
+    return update();
     });
     
     return div.textContent};
  const matrix = Array.from(gameboard.children).map(toText);
   
- return{matrix,player}
+ return{matrix,player,update}
 }
 )(); 
  
 
+const checkWinner = (function(){
+    
+
+
+
+})
 
